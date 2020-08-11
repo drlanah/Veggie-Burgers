d802c622f325ec99f141dc034c13e80966803457
drlanah <lanahuhko@gmail.com>
2020-08-10 20:53:17 -0400

    mysql, dotenv connections

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection(process.env.DB_URL);
+}
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
